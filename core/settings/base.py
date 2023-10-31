9249e253c83b7e0b24ba4e013423e42686d243a3
Pacifique Clement Ndayisenga <32579116+Paccy10@users.noreply.github.com>
2023-10-31 09:30:30 +0200

    [chore]: Add rate limiting - Add DRF throttling - Add running instructions

--- core/settings/base.py
+++ core/settings/base.py
@@ -162,6 +162,11 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 10,
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {"anon": "20/min", "user": "60/min"},
 }
 
 # Swagger
